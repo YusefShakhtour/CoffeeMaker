7d869b4a82092d284203339515f8d27a8151fe44
zbmarti2 <zbmarti2@ncsu.edu>
2024-03-28 11:27:54 -0400

    Add anonymous user functionality and comments

--- CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/models/User.java
+++ CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/models/User.java
@@ -1,7 +1,5 @@
 package edu.ncsu.csc.CoffeeMaker.models;
 
-import java.io.Serializable;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -9,51 +7,111 @@ import edu.ncsu.csc.CoffeeMaker.models.enums.UserType;
 
 public class User extends DomainObject {
 
+    /** User id */
     @Id
     @GeneratedValue
     private Long     id;
 
+    /** Name */
     private String   name;
 
+    /** Password */
     private String   pass;
 
+    /** User type */
     private UserType userType;
 
+    /**
+     * User constructor
+     *
+     * @param name
+     *            name
+     * @param pass
+     *            password
+     * @param userType
+     *            user type
+     */
     public User ( final String name, final String pass, final UserType userType ) {
         setName( name );
         setPassword( pass );
         setUserType( userType );
     }
 
+    /**
+     * User constructor for an anonymous user
+     */
+    public User () {
+        this( "", "", UserType.ANONYMOUS );
+    }
+
     @Override
-    public Serializable getId () {
+    public Long getId () {
         return id;
     }
 
+    /**
+     * Returns name
+     *
+     * @return name
+     */
     public String getName () {
         return name;
     }
 
+    /**
+     * Sets name to new name
+     *
+     * @param name
+     *            new name
+     */
     public void setName ( final String name ) {
         this.name = name;
     }
 
+    /**
+     * Returns password
+     *
+     * @return password
+     */
     public String getPassword () {
         return pass;
     }
 
+    /**
+     * Sets password to new password
+     *
+     * @param pass
+     *            new password
+     */
     public void setPassword ( final String pass ) {
         this.pass = pass;
     }
 
+    /**
+     * Returns user type
+     *
+     * @return user type
+     */
     public UserType getUserType () {
         return userType;
     }
 
+    /**
+     * Sets users type to new user type
+     *
+     * @param userType
+     *            new user type
+     */
     public void setUserType ( final UserType userType ) {
         this.userType = userType;
     }
 
+    /**
+     * Edits user with new info based on user parameter
+     *
+     * @param user
+     *            user parameter with new user info
+     */
     public void editUser ( final User user ) {
         setName( user.getName() );
         setPassword( user.getPassword() );
--- CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/UserType.java
+++ CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/UserType.java
@@ -6,5 +6,7 @@ public enum UserType {
     /** Barista */
     BARISTA,
     /** Manager */
-    MANAGER
+    MANAGER,
+    /** Anonymous */
+    ANONYMOUS
 }
