c12038f0d7bbc0340c2f8f249b896aba1b7ae726
Nmuckler <nathanmuckler@gmail.com>
2024-04-03 17:28:32 -0400

    Created UserTest.Java

---
+++ CoffeeMaker-Team/src/test/java/edu/ncsu/csc/CoffeeMaker/models/UserTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc.CoffeeMaker.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @Test
+    void test () {
+        fail( "Not yet implemented" );
+    }
+
+}
