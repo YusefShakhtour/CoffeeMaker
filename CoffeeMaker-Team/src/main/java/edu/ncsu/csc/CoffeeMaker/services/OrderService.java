01966bb3cbaaacd9d3d1569eede6e7731bd9d64b
Nmuckler <nathanmuckler@gmail.com>
2024-04-03 15:02:05 -0400

    Working on testing

--- CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/services/OrderService.java
+++ CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/services/OrderService.java
@@ -1,7 +1,10 @@
 package edu.ncsu.csc.CoffeeMaker.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import edu.ncsu.csc.CoffeeMaker.models.Order;
 import edu.ncsu.csc.CoffeeMaker.repositories.OrderRepository;
@@ -14,6 +17,8 @@ import edu.ncsu.csc.CoffeeMaker.repositories.OrderRepository;
  * @author Geigh Neill
  *
  */
+@Component
+@Transactional
 public class OrderService extends Service<Order, Long> {
 
     /**
--- CoffeeMaker-Team/src/test/java/edu/ncsu/csc/CoffeeMaker/models/OrderTest.java
+++ CoffeeMaker-Team/src/test/java/edu/ncsu/csc/CoffeeMaker/models/OrderTest.java
@@ -1,24 +1,38 @@
 package edu.ncsu.csc.CoffeeMaker.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-class OrderTest {
+import edu.ncsu.csc.CoffeeMaker.TestConfig;
+import edu.ncsu.csc.CoffeeMaker.services.OrderService;
 
+@ExtendWith ( SpringExtension.class )
+@EnableAutoConfiguration
+@SpringBootTest ( classes = TestConfig.class )
+public class OrderTest {
+
+    @Autowired
     private OrderService service;
 
     @BeforeEach
     public void setup () {
-        orderService.deleteAll();
+        service.deleteAll();
     }
 
     @Test
     @Transactional
     void testCreateOrder () {
+        final List<Recipe> recipes = new ArrayList<>();
         Assertions.assertEquals( 0, service.findAll().size(), "There should be no Orders in the CoffeeMaker" );
 
         final Recipe r1 = new Recipe( "Coffee", 15 );
@@ -27,7 +41,9 @@ class OrderTest {
         r1.addIngredient( new Ingredient( "Coffee", 1 ) );
         r1.addIngredient( new Ingredient( "Sugar", 1 ) );
 
-        final Order o1 = new Order( "Order 1", 10, false, r1 );
+        recipes.add( r1 );
+
+        final Order o1 = new Order( "Order 1", recipes );
         service.save( o1 );
 
         final String name2 = "Mocha";
@@ -36,34 +52,46 @@ class OrderTest {
         r2.addIngredient( new Ingredient( "Milk", 2 ) );
         r2.addIngredient( new Ingredient( "Sugar", 2 ) );
         r2.addIngredient( new Ingredient( "Chocolate", 1 ) );
+        recipes.add( r2 );
 
-        final Order o2 = new Order( "Order 2", 5, false, r2 );
+        final Order o2 = new Order( "Order 2", recipes );
         service.save( o2 );
 
         final List<Order> orders = service.findAll();
         Assertions.assertEquals( 2, orders.size(), "Creating two orders should result in two orders in the database" );
+        Assertions.assertEquals( 2, orders.get( 0 ).getName(), "Order 1" );
+        Assertions.assertEquals( 2, orders.get( 1 ).getName(), "Order 2" );
 
     }
 
     @Test
     @Transactional
-    void testCreateOrder () {
+    void test () {
+        final List<Recipe> recipes = new ArrayList<>();
         Assertions.assertEquals( 0, service.findAll().size(), "There should be no Orders in the CoffeeMaker" );
 
-        final Order o1 = new Order( "Order 1", 10 );
+        final Recipe r1 = new Recipe( "Coffee", 15 );
+        r1.addIngredient( new Ingredient( "Chocolate", 1 ) );
+        r1.addIngredient( new Ingredient( "Milk", -3 ) );
+        r1.addIngredient( new Ingredient( "Coffee", 1 ) );
+        r1.addIngredient( new Ingredient( "Sugar", 1 ) );
 
-        o1.setName( "Order 1" );
-        o1.setPayment( 10 );
+        recipes.add( r1 );
+
+        final Order o1 = new Order( "Order 1", recipes );
         service.save( o1 );
-        Assertions.assertEquals( o1.getName(), "Order 2" );
-        Assertions.assertEquals( o1.getPayment(), 10 );
 
-        final Order o2 = new Order();
+        final String name2 = "Mocha";
+        final Recipe r2 = new Recipe( name2, 40 );
+        r2.addIngredient( new Ingredient( "Coffee", 1 ) );
+        r2.addIngredient( new Ingredient( "Milk", 2 ) );
+        r2.addIngredient( new Ingredient( "Sugar", 2 ) );
+        r2.addIngredient( new Ingredient( "Chocolate", 1 ) );
+        recipes.add( r2 );
 
-        o2.setName( "Order 1" );
-        o2.setPayment( 5 );
+        final Order o2 = new Order( "Order 2", recipes );
         service.save( o2 );
-        Assertions.assertEquals( o2.getName(), "Order 2" );
+
         final List<Order> orders = service.findAll();
         Assertions.assertEquals( 2, orders.size(), "Creating two orders should result in two orders in the database" );
 
