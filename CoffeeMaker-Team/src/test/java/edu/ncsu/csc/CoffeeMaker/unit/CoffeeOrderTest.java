0898337ce41430c768d17cf3ecfa42298afee458
Yusef Shakhtour <yfshakht@ncsu.edu>
2024-04-03 19:19:01 -0400

    [CoffeeOrder Test]

--- CoffeeMaker-Team/src/test/java/edu/ncsu/csc/CoffeeMaker/unit/CoffeeOrderTest.java
+++ CoffeeMaker-Team/src/test/java/edu/ncsu/csc/CoffeeMaker/unit/CoffeeOrderTest.java
@@ -70,6 +70,59 @@ public class CoffeeOrderTest {
     }
 
     /**
+     * Tests the different types of constructors
+     */
+    @Test
+    @Transactional
+    public void testConstructors () {
+        final CoffeeOrder o1 = new CoffeeOrder();
+
+        Assertions.assertEquals( o1.getUser().getName(), "" );
+        Assertions.assertEquals( o1.getUser().getPassword(), "" );
+        Assertions.assertEquals( o1.getUser().getUserType(), UserType.ANONYMOUS );
+        Assertions.assertFalse( o1.getFulfilled() );
+
+        final String name1 = "Coffee";
+        final Recipe r1 = new Recipe( name1, 15 );
+        r1.addIngredient( new Ingredient( "Chocolate", 1 ) );
+        r1.addIngredient( new Ingredient( "Milk", 3 ) );
+        r1.addIngredient( new Ingredient( "Coffee", 2 ) );
+        r1.addIngredient( new Ingredient( "Sugar", 1 ) );
+        final String name2 = "Mocha";
+
+        final Recipe r2 = new Recipe( name2, 15 );
+
+        r2.addIngredient( new Ingredient( "Chocolate", 2 ) );
+        r2.addIngredient( new Ingredient( "Milk", 2 ) );
+        r2.addIngredient( new Ingredient( "Coffee", 1 ) );
+        r2.addIngredient( new Ingredient( "Sugar", 2 ) );
+
+        final List<Recipe> recipes = List.of( r1, r2 );
+
+        final CoffeeOrder o2 = new CoffeeOrder( recipes );
+        Assertions.assertEquals( o2.getUser().getName(), "" );
+        Assertions.assertEquals( o2.getUser().getPassword(), "" );
+        Assertions.assertEquals( o2.getUser().getUserType(), UserType.ANONYMOUS );
+        Assertions.assertFalse( o2.getFulfilled() );
+        Assertions.assertEquals( o2.getRecipes().size(), 2 );
+        Assertions.assertEquals( o2.getRecipes().get( 0 ), r1 );
+        Assertions.assertEquals( o2.getRecipes().get( 1 ), r2 );
+        Assertions.assertEquals( o2.getTotal(), 30 );
+
+        final User user1 = new User( "Andrew", "pass", UserType.CUSTOMER );
+        final CoffeeOrder o3 = new CoffeeOrder( user1, recipes );
+        Assertions.assertEquals( o3.getUser().getName(), "Andrew" );
+        Assertions.assertEquals( o3.getUser().getPassword(), "pass" );
+        Assertions.assertEquals( o3.getUser().getUserType(), UserType.CUSTOMER );
+        Assertions.assertFalse( o3.getFulfilled() );
+        Assertions.assertEquals( o3.getRecipes().size(), 2 );
+        Assertions.assertEquals( o3.getRecipes().get( 0 ), r1 );
+        Assertions.assertEquals( o3.getRecipes().get( 1 ), r2 );
+        Assertions.assertEquals( o3.getTotal(), 30 );
+
+    }
+
+    /**
      * Tests the to string method for recipe
      */
     @Test
@@ -92,6 +145,41 @@ public class CoffeeOrderTest {
     @Test
     @Transactional
     void testEqualsObject () {
+        Assertions.assertEquals( 0, service.findAll().size(), "There should be no Orders in the CoffeeMaker" );
+
+        final User user1 = new User( "Andrew", "pass", UserType.CUSTOMER );
+
+        final String name1 = "Coffee";
+        final Recipe r1 = new Recipe( name1, 15 );
+        r1.addIngredient( new Ingredient( "Chocolate", 1 ) );
+        r1.addIngredient( new Ingredient( "Milk", 3 ) );
+        r1.addIngredient( new Ingredient( "Coffee", 2 ) );
+        r1.addIngredient( new Ingredient( "Sugar", 1 ) );
+        final String name2 = "Mocha";
+        final Recipe r2 = new Recipe( name2, 15 );
+
+        r2.addIngredient( new Ingredient( "Chocolate", 2 ) );
+        r2.addIngredient( new Ingredient( "Milk", 2 ) );
+        r2.addIngredient( new Ingredient( "Coffee", 1 ) );
+        r2.addIngredient( new Ingredient( "Sugar", 2 ) );
+
+        final List<Recipe> recipes = List.of( r1, r2 );
+        final List<Recipe> recipes2 = List.of( r1, r2 );
+
+        final CoffeeOrder o1 = new CoffeeOrder();
+        o1.setUser( user1 );
+        o1.setRecipes( recipes );
+        service.save( o1 );
+
+        final CoffeeOrder o2 = new CoffeeOrder();
+        o2.setUser( user1 );
+        o2.setRecipes( recipes2 );
+        service.save( o2 );
+
+        final CoffeeOrder nullOrder = null;
+        Assertions.assertFalse( o2.equals( nullOrder ) );
+        Assertions.assertTrue( o2.equals( o2 ) );
+        Assertions.assertFalse( o2.equals( o1 ) );
 
     }
 
