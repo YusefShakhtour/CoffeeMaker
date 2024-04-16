2208420a810168f94d3b9cd2808d10a483725010
Nmuckler <nathanmuckler@gmail.com>
2024-04-16 19:54:33 -0400

    Instantiated the orders list

--- CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/models/User.java
+++ CoffeeMaker-Team/src/main/java/edu/ncsu/csc/CoffeeMaker/models/User.java
@@ -1,5 +1,6 @@
 package edu.ncsu.csc.CoffeeMaker.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -26,20 +27,20 @@ public class User extends DomainObject {
     /** User id */
     @Id
     @GeneratedValue
-    private Long              id;
+    private Long                    id;
 
     /** Name */
-    private String            name;
+    private String                  name;
 
     /** Password */
-    private String            password;
+    private String                  password;
 
     /** User type */
-    private UserType          userType;
+    private UserType                userType;
 
     /** list of orders for this user */
     @OneToMany ( cascade = CascadeType.MERGE, fetch = FetchType.EAGER )
-    private List<CoffeeOrder> orders;
+    private final List<CoffeeOrder> orders;
 
     /**
      * User constructor
@@ -56,6 +57,7 @@ public class User extends DomainObject {
         setPassword( pass );
         setUserType( userType );
 
+        this.orders = new ArrayList<>();
     }
 
     /**
